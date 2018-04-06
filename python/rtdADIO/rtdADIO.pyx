ce9f6bd11c47783ea28d5526146a712d73f0dac1
Craig Loomis <cloomis@astro.princeton.edu>
2018-04-05 22:13:20 -0400

    Allow connecting without initializing port bit directions.

--- python/rtdADIO/rtdADIO.pyx
+++ python/rtdADIO/rtdADIO.pyx
@@ -39,42 +39,51 @@ cdef class ADIO:
     cdef object _logger
     
     def __init__(self, mask, logger=None, logLevel=logging.DEBUG):
+        # logging.basicConfig(format='%(asctime)s %(levelname)s:%(message)s', level=logLevel)
+
         self.minor = 0          # Seems to be defined by the kernel module.
         self.portNum = 0
         self.port0mask = mask
         self.deviceHandle = <DeviceHandle> 0
-        if logger:
+        if logger is not None:
             self._logger = logger
         else:
             self._logger = logging.getLogger('adio')
         self._logger.setLevel(logLevel)
-        
-        self.connect()
+
+        self.connect(doInit=True)
 
     def __dealloc__(self):
         if self.deviceHandle != <DeviceHandle> 0:
             CloseDIO_aDIO(self.deviceHandle)
-        
-    def connect(self):
+
+    def logger(self):
+        return self._logger
+    
+    def connect(self, doInit=True):
+        self._logger.debug('opening aDIO device %d with mask=0x%02x',
+                           self.minor, self.port0mask)
         ret = OpenDIO_aDIO(&self.deviceHandle, self.minor)
         if ret != 0:
             raise RuntimeError("failed to open ADIO device. errno=%d" % ret)
 
         
         # Set output bits.
-        self._logger.debug('setting port0 bit mask to 0x%02x', self.port0mask)
-        ret = LoadPort0BitDir_aDIO(self.deviceHandle,
-                                   bool(self.port0mask & 0x80),
-                                   bool(self.port0mask & 0x40),
-                                   bool(self.port0mask & 0x20),
-                                   bool(self.port0mask & 0x10),
-                                   bool(self.port0mask & 0x08),
-                                   bool(self.port0mask & 0x04),
-                                   bool(self.port0mask & 0x02),
-                                   bool(self.port0mask & 0x01))
-        if ret != 0:
-            raise RuntimeError("failed to configure ADIO port 0. errno=%d" % ret)
-
+        if doInit:
+            self._logger.debug('setting port0 bit mask to 0x%02x', self.port0mask)
+            ret = LoadPort0BitDir_aDIO(self.deviceHandle,
+                                       bool(self.port0mask & 0x80),
+                                       bool(self.port0mask & 0x40),
+                                       bool(self.port0mask & 0x20),
+                                       bool(self.port0mask & 0x10),
+                                       bool(self.port0mask & 0x08),
+                                       bool(self.port0mask & 0x04),
+                                       bool(self.port0mask & 0x02),
+                                       bool(self.port0mask & 0x01))
+            if ret != 0:
+                raise RuntimeError("failed to configure ADIO port 0. errno=%d" % ret)
+            return self.status()
+        
     def disconnect(self):
         if self.deviceHandle != <DeviceHandle> 0:
             ret = CloseDIO_aDIO(self.deviceHandle)
@@ -114,8 +123,3 @@ cdef class ADIO:
         curMask = self.status()
 
         return self._set((curMask & ~bitMask) & self.port0mask)
-    
-    
-
-
-
